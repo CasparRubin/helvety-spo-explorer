0f7030e9b4e05bc18fa1c29e659bac8a95d821df
Caspar Rubin <caspar@helvety.com>
2026-01-27 15:36:55 +0100

    SPFx Init

---
+++ src/extensions/helvetySpoExplorer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IHelvetySpoExplorerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'HelvetySpoExplorerApplicationCustomizerStrings' {
+  const strings: IHelvetySpoExplorerApplicationCustomizerStrings;
+  export = strings;
+}
